81702518e747d9f8dc683db3ca937f4a7207b724
Max Tarasov <diexcry@mail.ru>
2020-02-24 13:46:20 +0300

    visual

--- UIScenes/MainUI.gd
+++ UIScenes/MainUI.gd
@@ -1,12 +1,16 @@
 extends Control
 
+var Train = preload("res://gameScenes/lvl1/level1.gd").new()
+
 func get_speed_instance():
 	return $"SpeedScroll"
 
 
-	
 
 
 func _on_Button_pressed() :
-	$Messages/Sprite.visible = !$Messages/Sprite.visible
+	$Messages/Window/.visible = !$Messages/Window/.visible
+
+
+
 
