f1ec4cb8331ef66537a20a8efd012d30ef80fe54
Max Tarasov <diexcry@mail.ru>
2020-03-01 12:37:13 +0300

    Revert "Merge branch 'develop' into Messeges"
    This reverts commit 2a3a9614312c582fe725d92593ae238a1069282a, reversing
    changes made to 81702518e747d9f8dc683db3ca937f4a7207b724.

--- UIScenes/MainUI.gd
+++ UIScenes/MainUI.gd
@@ -2,13 +2,12 @@ extends Control
 
 var Train = preload("res://gameScenes/lvl1/level1.gd").new()
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-var COLOR = null
-func get_speed_instance(color):
-	if color == COLOR:
-		return $"SpeedScroll"
+func get_speed_instance():
+	return $"SpeedScroll"
+
+
+
+
 func _on_Button_pressed() :
 	$Messages/Window/.visible = !$Messages/Window/.visible
 
